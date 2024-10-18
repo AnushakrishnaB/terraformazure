4ec7a408e328f218d3a8b8511649ef72fc3579c1
Anusha Besta <Anusha.Besta@lvs1.com>
2024-10-17 21:24:12 -0400

    added rg1 & rg2

---
+++ rg.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "devopsb28rg1" {
+  name     = "devopsb28rg1"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "devopsb28rg2" {
+  name     = "devopsb28rg2"
+  location = "eastus"
+}
