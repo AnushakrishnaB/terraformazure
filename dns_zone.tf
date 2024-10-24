0012435a27fcddac01225bba4650c98861aa19a9
Anusha Besta <Anusha.Besta@lvs1.com>
2024-10-24 04:47:43 -0400

    added dns record testserver1

--- dns_zone.tf
+++ dns_zone.tf
@@ -2,3 +2,11 @@ resource "azurerm_dns_zone" "devopsb28com" {
   name                = "devopsb28.com"
   resource_group_name = azurerm_resource_group.devopsb28tf.name
 }
+
+resource "azurerm_dns_a_record" "testserver1" {
+  name                = "testserver1"
+  zone_name           = azurerm_dns_zone.devopsb28com.name
+  resource_group_name = azurerm_resource_group.devopsb28tf.name
+  ttl                 = 300
+  records             = ["10.1.1.100"]
+}
